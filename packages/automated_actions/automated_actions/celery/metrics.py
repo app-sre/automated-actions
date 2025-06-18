87e95eb7b9083f8aea2825f5bab25db37bc828c6
Rafa Porres Molina <rporresm@redhat.com>
2025-06-18 18:26:01 +0200

    linting
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- packages/automated_actions/automated_actions/celery/metrics.py
+++ packages/automated_actions/automated_actions/celery/metrics.py
@@ -8,5 +8,21 @@ action_elapsed_time = Histogram(
     documentation="Elapsed seconds since the moment in the action was inserted in the action table, including retries.",
     labelnames=["name", "status"],
     registry=CELERY_REGISTRY,
-    buckets=(.05, .075, .1, .33, .66, 1.0, 2.0, 4.0, 6.0, 8.0, 10.0, 15.0, 20.0, 30.0, INF)
+    buckets=(
+        0.05,
+        0.075,
+        0.1,
+        0.33,
+        0.66,
+        1.0,
+        2.0,
+        4.0,
+        6.0,
+        8.0,
+        10.0,
+        15.0,
+        20.0,
+        30.0,
+        INF,
+    ),
 )
